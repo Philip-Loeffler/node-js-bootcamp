328ea4e3dcb5a020e22216171da4779b6de90f3d
philip-loeffler <philiploeffler@gmail.com>
2022-04-13 10:11:38 -0400

    fix folder name

--- Section2/ch9.js
+++ Section2/ch9.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-const replaceTemplate = require('./modules.js/replaceTemplate')
+const replaceTemplate = require('./modules/replaceTemplate')
 
 
 const replaceTemplate = (temp, product) => {
